7edb92fd16888046211862ce4781a0c7db0dbd7f
tguianet <tiagotguianet@gmail.com>
2026-08-26 21:59:12 -0300

    fix: support older mobile media query APIs

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -2,17 +2,34 @@ import * as React from "react";
 
 const MOBILE_BREAKPOINT = 768;
 
+type LegacyMediaQueryList = MediaQueryList & {
+  addListener?: (listener: (event: MediaQueryListEvent) => void) => void;
+  removeListener?: (listener: (event: MediaQueryListEvent) => void) => void;
+};
+
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    if (typeof window === "undefined" || typeof window.matchMedia !== "function") {
+      setIsMobile(false);
+      return;
+    }
+
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`) as LegacyMediaQueryList;
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
+
+    if (typeof mql.addEventListener === "function") mql.addEventListener("change", onChange);
+    else mql.addListener?.(onChange);
+
+    onChange();
+
+    return () => {
+      if (typeof mql.removeEventListener === "function") mql.removeEventListener("change", onChange);
+      else mql.removeListener?.(onChange);
+    };
   }, []);
 
   return !!isMobile;
